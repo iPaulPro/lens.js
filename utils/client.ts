3505c96752129a96db56dba0d1429fa2d1139168
Paul Burke <paul@paulburke.co>
2022-09-17 12:22:49 -0400

    Uses mumbai for the urql client

--- utils/client.ts
+++ utils/client.ts
@@ -2,5 +2,5 @@ import { createClient } from '@urql/core';
 import 'isomorphic-unfetch';
 
 export const client = createClient({
-  url: 'https://api.lens.dev',
+  url: 'https://api-mumbai.lens.dev',
 });
